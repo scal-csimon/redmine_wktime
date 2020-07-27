4298d79436199a9eddbcb63aea6bc023e4f49d31
scal-csimon <csimon@scal-informatique.com>
2020-07-27 19:51:13 +0000

    change description for crm object to 1000

---
+++ db/migrate/022_change_description_for_crm_object.rb
@@ -0,0 +1,40 @@
+class ChangeDescriptionForCrmObject < ActiveRecord::Migration[5.2]
+  def up
+
+    change_table :wk_crm_activities do |t|
+      t.change :description, :text, limit: 1000
+    end
+
+    change_table :wk_accounts do |t|
+      t.change :description, :text, limit: 1000
+    end
+
+    change_table :wk_crm_contacts  do |t|
+      t.change :description, :text, limit: 1000
+    end
+
+    change_table :wk_opportunities   do |t|
+      t.change :description, :text, limit: 1000
+    end
+
+
+  end
+  def down
+    change_table :wk_crm_activities do |t|
+      t.change :description, :string
+    end
+
+    change_table :wk_accounts do |t|
+      t.change :description, :string
+    end
+
+    change_table :wk_crm_contacts  do |t|
+      t.change :description, :string
+    end
+
+    change_table :wk_opportunities  do |t|
+      t.change :description, :string
+    end
+
+  end
+end

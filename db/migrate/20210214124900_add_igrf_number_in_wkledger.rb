a8f81d365b647fa8f519444b1b22fa2ef864eb34
scal-csimon <58978857+scal-csimon@users.noreply.github.com>
2021-02-14 13:35:57 -0500

    fix migration name

--- db/migrate/20210214124900_add_igrf_number_in_wkledger.rb
+++ db/migrate/20210214124900_add_igrf_number_in_wkledger.rb
@@ -1,4 +1,4 @@
-class AddIGRFNumberInWkLedger < ActiveRecord::Migration[5.2]
+class AddIgrfNumberInWkledger < ActiveRecord::Migration[5.2]
   
   def change
     add_column :wk_ledgers, :igrf_account_number, :integer

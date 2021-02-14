4814796be660f5a6746e85e1996e03547a553056
scal-csimon <58978857+scal-csimon@users.noreply.github.com>
2021-02-14 13:02:51 -0500

    Add igrf account on wkledger

---
+++ db/migrate/20210214124900_add_IGRF_number_in_WkLedger.rb
@@ -0,0 +1,7 @@
+class AddIGRFNumberInWkLedger < ActiveRecord::Migration[5.2]
+  
+  def change
+    add_column :wk_ledgers, :igrf_account_number, :integer
+    add_column :wk_ledgers, :igrf_account_description, :string
+  end
+end
